2d1ac4f5dcc33175bafff80062c426804d6ba8b1
mkheck13 <mkheck13@gmail.com>
2025-04-07 13:45:06 -0700

    redeploy

--- obj/Release/net8.0/fullstack-backend.AssemblyInfo.cs
+++ obj/Release/net8.0/fullstack-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fullstack-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a919f7fbf8c81d96ea8a7517d9546d630a93d47")]
 [assembly: System.Reflection.AssemblyProductAttribute("fullstack-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fullstack-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
