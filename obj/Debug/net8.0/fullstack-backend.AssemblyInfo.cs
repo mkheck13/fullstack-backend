2930144e4b6eaa2667207753c0e9f277842e68a9
mkheck13 <mkheck13@gmail.com>
2025-04-09 11:12:52 -0700

    changes

--- obj/Debug/net8.0/fullstack-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/fullstack-backend.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("fullstack-backend")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6569ba023aa0e4155d7370a073053313e6409e9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("fullstack-backend")]
