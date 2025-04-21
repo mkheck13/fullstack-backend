dcbf94f0a0634b733709f91c6d7155ac58621499
mkheck13 <mkheck13@gmail.com>
2025-04-21 14:36:28 -0700

    added to UserModel and UpdateUser

--- Models/DTOS/UpdateUserDTO.cs
+++ Models/DTOS/UpdateUserDTO.cs
@@ -7,5 +7,12 @@ namespace fullstack_backend.Models.DTOS
         public string? DateOfBirth { get; set; }
         public string? PhoneNumber { get; set; }
         public string? ProfilePicture { get; set; }
+        
+        // new updates
+        public string? UserBio { get; set; }
+        public string? UserLocation { get; set; }
+        public bool UserLocationPublic { get; set; }
+        public string? UserPrimarySport { get; set; }
+        public string? UserSecondarySport { get; set; }
     }
 }
