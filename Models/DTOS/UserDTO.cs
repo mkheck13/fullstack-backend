8ea61a1936630006da518394c40aa3dec7a2ebbb
mkheck13 <mkheck13@gmail.com>
2025-04-23 10:58:40 -0700

    userDTO update

--- Models/DTOS/UserDTO.cs
+++ Models/DTOS/UserDTO.cs
@@ -2,13 +2,19 @@ namespace fullstack_backend.Models.DTOS
 {
     public class UserDTO
     {
-        public string? Username {get; set;}
-        public string? Email { get; set;}
-        public string? Password {get; set;}
-        public string? DateOfBirth { get; set;}
-        public string? PhoneNumber { get; set;}
-        public bool IsTrainer { get; set;} 
-        public bool IsSpotter { get; set;} 
-        public string? TrueName { get; set;}
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? DateOfBirth { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? ProfilePicture { get; set; }
+        public string? UserBio { get; set; }
+        public string? UserLocation { get; set; }
+        public bool? UserLocationPublic { get; set; }
+        public string? UserPrimarySport { get; set; }
+        public string? UserSecondarySport { get; set; }
+        public bool IsTrainer { get; set; }
+        public bool IsSpotter { get; set; }
+        public string? TrueName { get; set; }
     }
 }
