a5c2059074206d75fd57c2fb4706181ec9b8a351
MundoDeSanti <santiagomm44@gmail.com>
2025-04-23 10:39:02 -0700

    SignalR preperations

--- Context/DataContext.cs
+++ Context/DataContext.cs
@@ -1,13 +1,14 @@
-using Microsoft.EntityFrameworkCore;
 using fullstack_backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace fullstack_backend.Context
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions options) : base (options)
-        {
-        }
-        public DbSet<UserModel> User { get; set;}
+        public DataContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<UserModel> User { get; set; }
+        public DbSet<MessageModel> Message { get; set; }
     }
 }
