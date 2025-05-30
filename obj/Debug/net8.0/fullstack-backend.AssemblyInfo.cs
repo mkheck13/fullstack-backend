90856b42ff526168bf72a5977b14e170c3d182dc
mkheck13 <mkheck13@gmail.com>
2025-05-30 08:33:35 -0700

    bluh

--- obj/Debug/net8.0/fullstack-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/fullstack-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fullstack-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcd98160d12c078a20fc8c81a121e5df1ac6165e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27867e4836cc83d289630992bb7edcd0d001093f")]
 [assembly: System.Reflection.AssemblyProductAttribute("fullstack-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fullstack-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
